230aa1c7cc64f5547960d07aaf7e7e063abeb6c6
devv-ho <khj199859@naver.com>
2025-08-03 15:46:02 +0900

    Fix clippy warning before converting to C++

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut a = String::new();
+
+    io::stdin().read_line(&mut a).expect("Failed to read");
+
+    let a: u32 = a.trim().parse().expect("it has to be a number");
+    println!("{a}");
 }
