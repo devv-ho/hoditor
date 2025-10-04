212653282caf65ad2b14a8a1a3a0e360a4243881
devv-ho <khj199859@naver.com>
2025-10-04 14:44:06 +0900

    Remove iced

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-#[derive(Default)]
-struct State;
+use iced::{Length, Task};
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Default)]
+struct TextEditorState {
+    content: iced::widget::text_editor::Content,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    ContentChanged(iced::widget::text_editor::Action),
+}
 
 fn main() -> iced::Result {
     iced::application("hoditor", update, view).run()
 }
 
-fn update(state: &mut State, message: Message) {
-    unimplemented!();
+fn update(text_editor: &mut TextEditorState, msg: Message) {
+    match msg {
+        Message::ContentChanged(action) => {
+            text_editor.content.perform(action);
+        }
+    }
 }
 
-fn view(state: &State) -> iced::Element<Message> {
-    iced::widget::text("Hello World!").into()
+fn view(text_editor: &TextEditorState) -> iced::Element<Message> {
+    iced::widget::text_editor(&text_editor.content)
+        .on_action(Message::ContentChanged)
+        .into()
 }
